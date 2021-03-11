23383ddd3251367dc4eae768bad457ae6cd0a9e4
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2021-03-11 22:05:52 +0200

    Update Blockchain.java

--- Blockchain.java
+++ Blockchain.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
  *
  * @author Heidi Holappa
  */
+
+// This class contains the list of Block objects and methods for handling the list 
 public class Blockchain {
     
     private ArrayList<Block> blockchain;
 
     public Blockchain() {
-        
         this.blockchain = new ArrayList<>();
     }
     
@@ -19,24 +20,25 @@ public class Blockchain {
         this.blockchain.add(block);
     }
     
-    public void getAll() {
-        
-        int size = blockchain.size();
-        
+    // Prints the objects in the list to the console
+    public void getAll() {    
+        int size = blockchain.size();    
         for (int i = 0; i < size; i++){
             System.out.println(blockchain.get(i).toString());
         }
             
     }
     
+    // Returns the String hash of the lastest Block on the list. 
     public String getPreviousHash() {
         int previousId = blockchain.size()-1;
         return blockchain.get(previousId).getHash();
     }
     
+    // This method initiates altering a previously entered data-item
+    // Gets a Block object and calls it's method alterData()
     public void alterData(int id, String data) {
         this.blockchain.get(id).alterData(data);
-        
     }
     
     public boolean isValid() {
