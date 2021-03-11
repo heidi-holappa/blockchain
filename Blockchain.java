4c0ea78f1bd64c1e4f020ea096f859043cac35bd
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2021-03-11 20:14:21 +0200

    Create Blockchain.java

---
+++ Blockchain.java
@@ -0,0 +1,59 @@
+package com.mycompany.blockchainsample;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Heidi Holappa
+ */
+public class Blockchain {
+    
+    private ArrayList<Block> blockchain;
+
+    public Blockchain() {
+        
+        this.blockchain = new ArrayList<>();
+    }
+    
+    public void addBlock(Block block) {
+        this.blockchain.add(block);
+    }
+    
+    public void getAll() {
+        
+        int size = blockchain.size();
+        
+        for (int i = 0; i < size; i++){
+            System.out.println(blockchain.get(i).toString());
+        }
+            
+    }
+    
+    public String getPreviousHash() {
+        int previousId = blockchain.size()-1;
+        return blockchain.get(previousId).getHash();
+    }
+    
+    public void alterData(int id, String data) {
+        this.blockchain.get(id).alterData(data);
+        
+    }
+    
+    public boolean isValid() {
+        int size = blockchain.size()-2;
+        for (int i = 0; i < size; i++) {
+            if (!blockchain.get(i).getHash().equals(blockchain.get(i+1).getPreviousHash())) {
+                return false; 
+            }        
+            
+        }
+        
+        return true; 
+        
+    }
+    
+    public int generateId() {
+        return this.blockchain.size();
+    }
+    
+}
