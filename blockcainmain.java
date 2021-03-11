7278f28767757a580efad0550a3a4e19c98083bf
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2021-03-11 16:23:24 +0200

    Update blockcainmain.java

--- blockcainmain.java
+++ blockcainmain.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class blockchainMain {
 
 	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+		
+		// Create a simple console-UI for submitting blocks and then options to violate the chain and to see what happens
     		
 	}
     
