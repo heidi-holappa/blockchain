0a26bce71ecc95cd2a9b617e961764ada0114d9f
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2021-03-13 18:08:18 +0200

    Update Block.java

--- Block.java
+++ Block.java
@@ -3,6 +3,7 @@ package com.mycompany.blockchainsample;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import java.security.MessageDigest;
+import java.lang.System;
 import java.util.Date;
 import java.sql.Timestamp;
 
@@ -17,7 +18,7 @@ public class Block {
 
     private int id;
     private String data;
-    private String privateKey;
+    private int nonce;
     private String hash;
     private String previousHash;
     public long date; 
@@ -29,7 +30,7 @@ public class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.date = date;
-        this.hash = calculateBlockHash();
+        this.hash = "NOTMINED";
 
     // call a method that creates the public key and perhaps a private key? 
     
@@ -39,11 +40,15 @@ public class Block {
     public String toString() {
         return "ID: " + this.id 
                 + " / DATA: " + this.data 
-                + " / PREVIOUSHASH " + this.previousHash 
-                + " / HASH: " + this.hash 
-                + " / DATE " + this.date;
+                + " / PREVIOUS_HASH " + this.previousHash
+                + " / NONCE: " + this.nonce
+                + " / DATE " + this.date
+                + " / HASH: " + this.hash;
     }
     
+    // This method is used when the block has already been mined
+    // The nonce is set in a way that the hash starts with an amount of zeros that equals to the prefix at the time
+    // Just calculating the has is quite fast. 
     public String getHash() {
         return calculateBlockHash();
     }
@@ -52,16 +57,30 @@ public class Block {
         return this.previousHash;
     }
     
-    public void alterData(String data) {
+    public void alterData(String data, int prefix) {
         this.data = data;
-        this.hash = calculateBlockHash();
+        this.hash = "NOTMINED";
+        this.hash = mineBlock(prefix);
+    }
+    
+    // Taken from Baeldungs example
+    // This method does the mining based on the set prefix
+    // By default the prefix is 4 which means that there needs to be 4 zeros in the beginning of the hash. 
+    public String mineBlock(int prefix) {
+        String prefixString = new String(new char[prefix]).replace('\0', '0');
+        while (!hash.substring(0, prefix).equals(prefixString)) {
+            nonce++;
+            hash = calculateBlockHash();
+        }
+
+        return hash; 
     }
   
 
   
     // Taken and slightly altered from the Baeldung tutorial on blockchain
     public String calculateBlockHash() {
-        String dataToHash = this.previousHash + this.date + this.data;
+        String dataToHash = previousHash + Long.toString(date) + Integer.toString(nonce) +  data;
         
         MessageDigest digest = null;
         byte[] bytes = null;
