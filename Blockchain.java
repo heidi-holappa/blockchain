20d48c8556c768a5f5d407972faebd84f36773ee
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2021-03-13 18:09:11 +0200

    Update Blockchain.java

--- Blockchain.java
+++ Blockchain.java
@@ -11,9 +11,13 @@ import java.util.ArrayList;
 public class Blockchain {
     
     private ArrayList<Block> blockchain;
+    private int prefix;
 
+    // Creates the blockchain. Prefix is set to 4 by default. 
     public Blockchain() {
         this.blockchain = new ArrayList<>();
+        this.prefix = 4;
+        
     }
     
     public void addBlock(Block block) {
@@ -38,9 +42,11 @@ public class Blockchain {
     // This method initiates altering a previously entered data-item
     // Gets a Block object and calls it's method alterData()
     public void alterData(int id, String data) {
-        this.blockchain.get(id).alterData(data);
+        this.blockchain.get(id).alterData(data, this.prefix);
     }
     
+    // In this demo the hash is also altered when data is altered
+    // Even if the hash was not altered, this method would still spot the change in the chain.
     public boolean isValid() {
         int size = blockchain.size()-1;
         for (int i = 0; i < size; i++) {
@@ -56,6 +62,21 @@ public class Blockchain {
     
     public int generateId() {
         return this.blockchain.size();
+    }
+    
+    public int getPrefix() {
+        return this.prefix;
+    }
+    
+    // Even with 8 zeros it takes quite a while to 'mine' the block.
+    public void setPrefix(int n) {
+        if (0 < n && n <9) {
+            this.prefix = n;
+            
+        } else {
+            System.out.println("PLEASE INSERT VALUE BETWEEN 1-8 ");
+        }
+        
     }
     
 }
