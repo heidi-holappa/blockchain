92a01382ba1dbc70618094c4b3b5ba87ee8c95e4
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2021-03-13 18:08:55 +0200

    Update BlockchainMain.java

--- BlockchainMain.java
+++ BlockchainMain.java
@@ -1,82 +1,180 @@
-package com.mycompany.blockchainsample;
 
-import java.util.ArrayList;
+package com.mycompany.blockchainsample;
+import java.util.Date;
+import java.sql.Timestamp;
 
 /**
  *
  * @author Heidi Holappa
  */
 
-// This class contains the list of Block objects and methods for handling the list 
-public class Blockchain {
-    
-    private ArrayList<Block> blockchain;
-    private int prefix;
+import java.util.Scanner;
 
-    // Creates the blockchain. Prefix is set to 4 by default. 
-    public Blockchain() {
-        this.blockchain = new ArrayList<>();
-        this.prefix = 4;
+public class BlockchainMain {
+    
+    public static void main(String[] args) {
         
-    }
-    
-    public void addBlock(Block block) {
-        this.blockchain.add(block);
-    }
-    
-    // Prints the objects in the list to the console
-    public void getAll() {    
-        int size = blockchain.size();    
-        for (int i = 0; i < size; i++){
-            System.out.println(blockchain.get(i).toString());
-        }
+        // Create an ArrayList for the blocks
+        Blockchain blockchain = new Blockchain();
+        
+        // create a genesis-block
+        int genesisId = 0;
+        String genesisData = "GENESIS_BLOCK";
+        String genesisPreviousHash = "NA";
+        Date genesisGetDate = new Date();
+        long genesisTimestamp = genesisGetDate.getTime();
+        
+        Block genesisBlock = new Block(genesisId, genesisData, genesisPreviousHash, genesisTimestamp);
+        genesisBlock.mineBlock(blockchain.getPrefix());
+        // add genesis-block to the ArrayList
+        blockchain.addBlock(genesisBlock);
+        
+        while (true) {
+            System.out.println("1: ADD BLOCK");
+            System.out.println("2: ADD MULTIPLE BLOCKS");
+            System.out.println("3: CHECK VALIDITY");
+            System.out.println("4: ALTER DATA");
+            System.out.println("5: PRINT BLOCKS");
+            System.out.println("6: ALTER PREFIX");
+            System.out.println("7: DEMO PROOF OF WORK WITH PREFIX 1-6");
+            System.out.println("0: END");
             
-    }
-    
-    // Returns the String hash of the lastest Block on the list. 
-    public String getPreviousHash() {
-        int previousId = blockchain.size()-1;
-        return blockchain.get(previousId).getHash();
-    }
-    
-    // This method initiates altering a previously entered data-item
-    // Gets a Block object and calls it's method alterData()
-    public void alterData(int id, String data) {
-        this.blockchain.get(id).alterData(data, this.prefix);
-    }
-    
-    // In this demo the hash is also altered when data is altered
-    // Even if the hash was not altered, this method would still spot the change in the chain.
-    public boolean isValid() {
-        int size = blockchain.size()-1;
-        for (int i = 0; i < size; i++) {
-            if (!blockchain.get(i).getHash().equals(blockchain.get(i+1).getPreviousHash())) {
-                return false; 
-            }        
+            Scanner reader = new Scanner(System.in);
+            String input = reader.nextLine();
+            
+            // Add a single block to the chain
+            if (input.equals("1")) {
+                System.out.print("ENTER DATA: ");
+                String blockData = reader.nextLine();
+                
+                long startTime = System.nanoTime();
+                
+                int blockId = blockchain.generateId();
+                String blockPreviousHash = blockchain.getPreviousHash();
+                Date blockDate = new Date();
+                long blockTimestamp = genesisGetDate.getTime();
+                Block block = new Block(blockId, blockData, blockPreviousHash, blockTimestamp);
+                block.mineBlock(blockchain.getPrefix());
+                blockchain.addBlock(block);
+                
+                long endTime = System.nanoTime();
+                long elapsedTime = (endTime - startTime) / 1000000;
+                System.out.println("####");
+                System.out.println("ELAPSED TIME: " + elapsedTime + " MILLISECONDS");
+                System.out.println("####");
+                continue; 
+            
+            // Add multiple blocks with automatically generated data-entry.
+            } else if (input.equals("2")) {
+                System.out.print("NUMBER OF BLOCKS TO BE ADDED: ");
+                int number = Integer.valueOf(reader.nextLine());
+                
+                long startTime = System.nanoTime();
+                
+                for (int i = 0; i < number; i++) {
+                    int blockId = blockchain.generateId();
+                    String blockData = "DATA ENTRY FOR ID " + blockId;
+                    String blockPreviousHash = blockchain.getPreviousHash();
+                    Date blockDate = new Date();
+                    long blockTimestamp = genesisGetDate.getTime();
+                    Block block = new Block(blockId, blockData, blockPreviousHash, blockTimestamp);
+                    block.mineBlock(blockchain.getPrefix());
+                    blockchain.addBlock(block);
+                }
+                
+                long endTime = System.nanoTime();
+                long elapsedTime = (endTime - startTime) / 1000000;
+                System.out.println("####");
+                System.out.println("ELAPSED TIME: " + elapsedTime + " MILLISECONDS");
+                System.out.println("####");
+                
+                continue; 
+
+            // Check is chain valid. For block's which id is between 0 <= id < n, where n = (blockchain.size() - 1), the previousHash of block m+1 must equal the hash of block m. 
+            } else if (input.equals("3")) {
+                System.out.println("---------------");
+                System.out.println("CHAIN VALID: " + blockchain.isValid());
+                System.out.println("---------------");
+            
+            // Change data on selected block to see what happens to the chain. 
+            } else if (input.equals("4")) {
+                System.out.print("ENTER ID: ");
+                int alterId = Integer.valueOf(reader.nextLine());
+                System.out.print("ENTER DATA: ");
+                String newData = reader.nextLine();
+                if (alterId < 0 || alterId > blockchain.generateId()-1) {
+                    System.out.println("ERROR. INVALID ID. PLEASE TRY AGAIN");
+                    continue;
+                } else if (alterId == blockchain.generateId()-1) {
+                    System.out.println("---------------");
+                    System.out.println("WARNING! ID CAN NOT BE THE LAST BLOCK IN THE CHAIN. PLEASE TRY AGAIN");
+                    System.out.println("---------------");
+                    continue; 
+                }
+                blockchain.alterData(alterId, newData);                
+                System.out.println("---------------");
+                System.out.println("DATA ALTERED.");
+                System.out.println("---------------");
+                continue;
+            
+            // Print all items in the ArrayList blockchain to console.     
+            } else if (input.equals("5")) {
+                blockchain.getAll();
+                continue; 
+            } else if (input.equals("6")) {
+                System.out.print("ENTER VALUE BETWEEN 1-8: ");
+                int newPrefix = Integer.valueOf(reader.nextLine());
+                blockchain.setPrefix(newPrefix);
+                continue; 
+            // Calculates the 'cost' of finding a hash with the set prefix by brute force.
+            // The cost is starting to really show with prefix values 4 and higher. 
+            // On my old machine (HP Elitebook 1040 G1) it takes app. 10 minutes to mine one block with prefix 6. 
+            } else if (input.equals("7")) {
+                int newPrefix = 1; 
+                blockchain.setPrefix(newPrefix);
+                for (int i=0; i<6; i++) {
+                    long startTime = System.nanoTime();
+                    
+                    
+                    int blockId = blockchain.generateId();
+                    String blockData = "DATA ENTRY FOR ID " + blockId;
+                    String blockPreviousHash = blockchain.getPreviousHash();
+                    Date blockDate = new Date();
+                    long blockTimestamp = genesisGetDate.getTime();
+                    Block block = new Block(blockId, blockData, blockPreviousHash, blockTimestamp);
+                    block.mineBlock(blockchain.getPrefix());
+                    blockchain.addBlock(block);
+                    
+                    
+                    long endTime = System.nanoTime();
+                    long elapsedTime = (endTime - startTime) / 1000000;
+                    System.out.println("####");
+                    System.out.println("PREFIX: " + newPrefix);
+                    System.out.println("ELAPSED TIME: " + elapsedTime + " MILLISECONDS");
+                    System.out.println("####");
+                    System.out.println("CREATED BLOCK: "+ block.toString());
+                    System.out.println("####");
+                    
+                    newPrefix++;
+                    blockchain.setPrefix(newPrefix);
+                    
+                    
+                }
+                
+                blockchain.setPrefix(4);
+                continue;
+            
+            } else if (input.equals("0")) {
+                break;
+                
+            } else {
+                System.out.println("---------------");
+                System.out.println("INVALID COMMAND. TRY AGAIN. PRESS 0 TO END.");
+                System.out.println("---------------");
+            }
+            
             
         }
-        
-        return true; 
-        
+
     }
-    
-    public int generateId() {
-        return this.blockchain.size();
-    }
-    
-    public int getPrefix() {
-        return this.prefix;
-    }
-    
-    // Even with 8 zeros it takes quite a while to 'mine' the block.
-    public void setPrefix(int n) {
-        if (0 < n && n <9) {
-            this.prefix = n;
-            
-        } else {
-            System.out.println("PLEASE INSERT VALUE BETWEEN 1-8 ");
-        }
-        
-    }
-    
 }
